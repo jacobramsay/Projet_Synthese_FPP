4663fd28db7fe8a0f5a672eb04f5ef7b3d9cb399
Jacob Ramsay <ramsayjacob@gmail.com>
2018-09-13 08:46:40 -0400

    commit

---
+++ Projet_synthese_FPP/Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Mover : MonoBehaviour
+    {
+        public void move(float speed)
+        {
+            
+        }
+    }
+}
---
+++ Projet_synthese_FPP/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Assets.Scripts;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 1;
+
+    private Mover mover;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+    void Awake()
+    {
+        mover = GetComponent<Mover>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKey(KeyCode.LeftArrow))
+	    {
+	        mover.move(speed);
+	    }
+	    if (Input.GetKeyDown(KeyCode.UpArrow))
+	    {
+	        mover.move(speed);
+        }
+	    if (Input.GetKeyDown(KeyCode.DownArrow))
+	    {
+	        mover.move(speed);
+        }
+	    if (Input.GetKeyDown(KeyCode.RightArrow))
+	    {
+	        mover.move(speed);
+        }
+        
+    }
+}
