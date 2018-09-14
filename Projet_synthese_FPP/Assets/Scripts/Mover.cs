1ec7d2788743b33f1a2f01806dd525563ea3ba5e
Nicolas Dionne <nicolas_dionne94@hotmail.com>
2018-09-14 09:08:48 -0400

    Added movments, and wall jump

--- Projet_synthese_FPP/Assets/Scripts/Mover.cs
+++ Projet_synthese_FPP/Assets/Scripts/Mover.cs
@@ -9,6 +9,32 @@ namespace Assets.Scripts
 {
     class Mover : MonoBehaviour
     {
+        private Rigidbody2D rigidBody;
+
+
+        private void Start()
+        {
+            rigidBody = transform.root.GetComponentInParent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float speed = 10;
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = 15;
+            }
+            else
+            {
+                speed = 7;
+            }
+            HandleMovement(horizontal, speed);
+        }
+        public void HandleMovement(float horizontal,float speed)
+        {
+            rigidBody.velocity = new Vector2(horizontal * speed, rigidBody.velocity.y);
+        }
         public void move(float speed)
         {
             
